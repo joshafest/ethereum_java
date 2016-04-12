303977c2254a72862e5024d26051f7e2c0e4b0c5
joshafest <joshaf.est@gmail.com>
2016-04-12 17:05:14 +0530

    inital comment

---
+++ org/ethereum/core/Block.java
@@ -0,0 +1,14 @@
+package org.ethereum.core
+
+import org.slf4j.Logger; 
+import org.slf4j.LoggerFactory; 
+import org.spongycastle.util.Arrays; 
+import org.spongycastle.util.encoders.Hex; 
+
+ 
+import java.math.BigInteger; 
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.concurrent.CopyOnWriteArrayList; 
+
+public class Block
